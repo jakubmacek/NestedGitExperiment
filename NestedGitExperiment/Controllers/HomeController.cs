a453d8d3320346dbe23ee053eeed5c1c71b3f8fe
Jakub Macek <jakub@poski.com>
2016-03-17 12:09:53 +0100

    Integration with Git.

--- NestedGitExperiment/Controllers/HomeController.cs
+++ NestedGitExperiment/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,22 @@ namespace NestedGitExperiment.Controllers
         public ActionResult Index()
         {
             var textPath = Server.MapPath("~/Assets/text.txt");
-            //ViewBag.Text = System.IO.File.ReadAllText(textPath);
+            ViewBag.Text = System.IO.File.ReadAllText(textPath);
             return View();
+        }
+
+        public ActionResult UpdateAssets()
+        {
+            var assetsPath = Server.MapPath("~/Assets");
+            using (var repo = new Repository(assetsPath))
+            {
+                repo.Fetch("origin", new FetchOptions() { TagFetchMode = TagFetchMode.None });
+                var remoteBranch = repo.Branches.First(x => x.FriendlyName == "origin/master");
+                var signature = new Signature("Jakub Macek", "1@jakubmacek.cz", DateTimeOffset.Now);
+                var mergeResult = repo.Merge(remoteBranch, signature);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
