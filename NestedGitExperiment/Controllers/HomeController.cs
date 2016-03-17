28edd661f4c2ba22d9f1481c8ac1b489ba76c60a
Jakub Macek <jakub@poski.com>
2016-03-17 11:12:25 +0100

    Version 1.

---
+++ NestedGitExperiment/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NestedGitExperiment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var textPath = Server.MapPath("~/Assets/text.txt");
+            //ViewBag.Text = System.IO.File.ReadAllText(textPath);
+            return View();
+        }
+    }
+}
---
+++ NestedGitExperiment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NestedGitExperiment.MvcApplication" Language="C#" %>
